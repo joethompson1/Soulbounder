a068a6435157640ae153ce8790a50a7f5c34926d
joethompson1 <thompsonjoejj@gmail.com>
2023-03-18 17:06:02 -0300

    created viewSBT page

--- Website/controllers/libraryController.js
+++ Website/controllers/libraryController.js
@@ -2,7 +2,14 @@
 
 // controller actions
 export const library_get = async (req, res) => {
-	res.status(200).render('library/viewSBT');
+	const request = new Request("https://ipfs.io/ipfs/"+req.query.hash);
+	const response = await fetch(request);
+	const SBTData = await response.json();
+	console.log(SBTData);
+
+	// console.log(req.query.hash);
+
+	res.status(200).render('library/viewSBT', { SBTData });
 };
 
 
