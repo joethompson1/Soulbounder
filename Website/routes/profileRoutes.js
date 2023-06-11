023e5550583953bbb9a2d9a96806cd9b867c8e38
joethompson <thompsonjoejj@gmail.com>
2023-06-11 18:04:25 +0100

    fixed routes bug when removed function

--- Website/routes/profileRoutes.js
+++ Website/routes/profileRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
-import { profile_get, decryptAuthToken, edit_get, edit_post } from '../controllers/profileController.js';
+import { profile_get, edit_get, edit_post } from '../controllers/profileController.js';
 
 
 router.get('/', profile_get);
-router.post('/decryptAuthToken', decryptAuthToken);
 router.get('/edit', edit_get);
 router.post('/edit', edit_post);
 
